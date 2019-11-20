2f7c724311a2018333d2688a84ec15cb6d933b67
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2019-11-20 18:27:22 +0430

    updated js

--- js/app.js
+++ js/app.js
@@ -55,6 +55,7 @@ if(scores[activePlayer] >= 10){
   document.querySelector('#name-' + activePlayer).textContent = "Winner!!";
   document.querySelector('.dice').style.display = "none";
   document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+  document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
 }else {
   //next player
   nextPlayer();
